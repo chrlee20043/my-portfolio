f2bb7de3e9c4086da99889aff8f07009d779c3bc
Christina Lee <chrlee93@gmail.com>
2023-11-29 12:17:57 -0800

    about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,63 @@
 import React from "react";
 import "./about.css";
+import ME from "../../assets/me-about.jpeg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section id="about">
+      <h2>Get to Know Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Me" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>6+ Years Working</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>300+ Worldwide</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+          <p>
+            My passion for web development started in 2019 when I first learned
+            HTML to update and maintain my employer's website at a nonprofit. I
+            continued to develop my skills a Client Support Specialist at a SaaS
+            startup where I worked closely with clients and engineers and
+            developed more technical skills, starting with SQL.
+            <br />
+            <br />
+            I recently graduated from the Grace Hopper coding bootcamp at
+            Fullstack Academy. I really enjoyed the collaborative learning
+            environment and working independently and on a team to build apps
+            and learn new technologies.
+            <br />
+            <br />
+            I'm currently looking for new opportunities, please reach out to
+            connect!
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
