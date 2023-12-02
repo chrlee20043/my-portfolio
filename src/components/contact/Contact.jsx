3c83ad36dcb0231e5a1dd5de92a649a5df14f0f9
Christina Lee <chrlee93@gmail.com>
2023-12-01 19:19:02 -0800

    contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,87 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_cq0w13a",
+        "template_68knuym",
+        form.current,
+        "1RjqFWglZ1rRdoCso"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>chrlee93@gmail.com</h5>
+            <a
+              href="mailto:chrlee93@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <AiOutlineLinkedin className="contact__option-icon" />
+            <h4>LinkedIn</h4>
+            <h5>chrlee93@gmail.com</h5>
+            <a
+              href="https://www.linkedin.com/in/christinaehlee/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Connect
+            </a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
